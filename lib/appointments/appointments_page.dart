547fe631f36da738673a5ac217f461422c746d65
wiktoriasobczyk <wiktoria.sobczyk@htdevelopers.com>
2021-06-12 00:18:44 +0200

    provide appointment bloc to page

--- lib/appointments/appointments_page.dart
+++ lib/appointments/appointments_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:health_app/appointments/bloc/appointment_bloc.dart';
 
 class AppointmentsPage extends StatelessWidget {
   const AppointmentsPage({
@@ -8,10 +10,12 @@ class AppointmentsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Appointments')),
-      body: Center(
-        child: Text('Appointments screen'),
-      ),
-    );
+        appBar: AppBar(title: const Text('Appointments')),
+        body: BlocProvider(
+          create: (context) => AppointmentBloc(),
+          child: Center(
+            child: Text('Appointments screen'),
+          ),
+        ));
   }
 }
